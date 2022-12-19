e87a81da0b87b962a843883826bca09b2e763f68
Rark7040 <crimera.hello@gmail.com>
2022-12-19 22:22:48 +0900

    check top block

--- src/rarkhopper/athletic/listener/handler/SlidingHandlerTrait.php
+++ src/rarkhopper/athletic/listener/handler/SlidingHandlerTrait.php
@@ -24,9 +24,11 @@ trait SlidingHandlerTrait{
 	
 	public function onToggleSwim(PlayerToggleSwimEvent $ev):void{
 		$athleticPlayer = AthleticPlayerMap::getInstance()->get($ev->getPlayer());
+		$player = $athleticPlayer->getPlayer();
 		$attr = $athleticPlayer->getAttribute();
+		$topBlock = $player->getWorld()->getBlock($player->getPosition()->add(0, 1, 0));
 		
-		if($attr->keepSliding){
+		if($attr->keepSliding and !$topBlock->isSolid()){
 			//頭上のブロックがsolidではないかつ、水中ではないのでkeep終了
 			if($ev->isSwimming()) return;
 			$attr->keepSliding = false;
